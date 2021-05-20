755aa628c0b7e3f6478947cd28423e403326a11f
Miguel <mrcb08022002@gmail.com>
2021-05-21 00:05:46 +0100

    add projects

---
+++ index.js
@@ -0,0 +1,11 @@
+function dispbox2()
+{
+    document.getElementById("box1").style.display="none";
+    document.getElementById("box2").style.display="block"
+}
+
+function closebox2()
+{
+    document.getElementById("box2").style.display="none";
+    document.getElementById("box1").style.display="block" 
+}
